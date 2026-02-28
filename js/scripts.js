3166a765a093e3ecc27e5fe4ff72a939cdd8f88f
Salvador Ursua <118657557+SalvadorUrsua@users.noreply.github.com>
2026-02-28 22:29:15 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,20 @@
-function showMenu() {
-  const header = document.querySelector(".header");
-  const iconButtons = document.querySelectorAll(".icon-mobile-nav");
-  const mobileBtn = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+const iconButtons = document.querySelectorAll(".icon-mobile-nav");
+const mobileBtn = document.querySelector(".btn-mobile-nav");
+
+iconButtons[0].addEventListener("click", () => {
   header.classList.add("menu-open");
-  iconButtons[0].style.display = "none";
-  iconButtons[1].style.display = "block";
   mobileBtn.setAttribute("focus", false);
-}
+});
 
-function hideMenu() {
-  const header = document.querySelector(".header");
-  const iconButtons = document.querySelectorAll(".icon-mobile-nav");
+iconButtons[1].addEventListener("click", () => {
   header.classList.remove("menu-open");
-  iconButtons[0].style.display = "block";
-  iconButtons[1].style.display = "none";
-}
+});
 
-function clearMenu() {
-  const header = document.querySelector(".header");
-  const iconButtons = document.querySelectorAll(".icon-mobile-nav");
-  header.classList.remove("menu-open");
-  iconButtons[0].style.display = "block";
-  iconButtons[1].style.display = "none";
-}
+const linkButton = document.querySelectorAll(".header-nav .nav-link");
+linkButton.forEach((e) => {
+  e.addEventListener("click", () => {
+    header.classList.remove("menu-open");
+    alert(e.className);
+  });
+});
