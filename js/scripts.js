16b5e76e57243c8adff994c8566b9938dd66d8d0
Salvador Ursua <118657557+SalvadorUrsua@users.noreply.github.com>
2026-03-01 00:20:17 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const header = document.querySelector(".header");
+const iconButtons = document.querySelectorAll(".icon-mobile-nav");
+const mobileBtn = document.querySelector(".btn-mobile-nav");
+
+iconButtons[0].addEventListener("click", () => {
+  header.classList.add("menu-open");
+  mobileBtn.setAttribute("focus", false);
+});
+
+iconButtons[1].addEventListener("click", () => {
+  header.classList.remove("menu-open");
+});
+
+const linkButton = document.querySelectorAll(".header-nav .nav-link");
+linkButton.forEach((e) => {
+  e.addEventListener("click", () => {
+    header.classList.remove("menu-open");
+  });
+});
+
+// alert(window.innerWidth);
